e6e29a53982e225cce64496edd739344891091ae
Tweni47 <derrickayaya@gmail.com>
2024-06-19 13:52:26 +0300

    Module 04 Lab Complete

--- code/module-04/m04-start/module04.js
+++ code/module-04/m04-start/module04.js
@@ -1,11 +1,10 @@
-"use strict";
 /* Module 4: Develop typed functions using TypeScript
    Lab Start  */
 /*  TODO: Convert the sortDescending and sortAscending functions to arrow
     functions. */
 /*  sortDescending is a comparison function that tells the sort method how to sort
     numbers in descending order */
-const sortDescending = (a, b) => {
+var sortDescending = function (a, b) {
     if (a > b) {
         return -1;
     }
@@ -18,7 +17,7 @@ const sortDescending = (a, b) => {
 };
 /*  sortDescending is a comparison function that tells the sort method how to sort
     numbers in ascending order. */
-const sortAscending = (a, b) => {
+var sortAscending = function (a, b) {
     if (a > b) {
         return 1;
     }
@@ -34,9 +33,9 @@ const sortAscending = (a, b) => {
     whether to sort the array in ascending or descending order. */
 /*  TODO: Update the BuildArray function. */
 function buildArray(items, sortOrder) {
-    let randomNumbers = [];
-    let nextNumber;
-    for (let counter = 0; counter < items; counter++) {
+    var randomNumbers = [];
+    var nextNumber;
+    for (var counter = 0; counter < items; counter++) {
         nextNumber = Math.ceil(Math.random() * (100 - 1));
         if (randomNumbers.indexOf(nextNumber) === -1) {
             randomNumbers.push(nextNumber);
@@ -52,15 +51,16 @@ function buildArray(items, sortOrder) {
         return randomNumbers.sort(sortDescending);
     }
 }
-let myArray1 = buildArray(12, "ascending");
-let myArray2 = buildArray(8, "descending");
+var myArray1 = buildArray(12, "ascending");
+var myArray2 = buildArray(8, "descending");
 console.log(myArray1);
 console.log(myArray2);
 /*  EXERCISE 2
     TODO: Update the LoanCalculator function. */
-function loanCalculator(principle, interestRate, months = 12) {
-    let interest = interestRate / 1200; // Calculates the monthly interest rate
-    let payment;
+function loanCalculator(principle, interestRate, months) {
+    if (months === void 0) { months = 12; }
+    var interest = interestRate / 1200; // Calculates the monthly interest rate
+    var payment;
     payment = (principle * interest) / (1 - Math.pow(1 / (1 + interest), months));
     return payment.toFixed(2);
 }
